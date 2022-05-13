74e25d8792667342a97c7542a3f1d4ebce20d98e
Dmitry <7897044@bk.ru>
2022-05-13 12:05:38 +0300

    Add functional2

--- src/main/java/by/autosearch/controllers/AutoSearchController.java
+++ src/main/java/by/autosearch/controllers/AutoSearchController.java
@@ -1,9 +1,9 @@
 package by.autosearch.controllers;
 
 import by.autosearch.dto.ParamSearchDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/api/auto-search")
@@ -12,5 +12,20 @@ public class AutoSearchController {
     @GetMapping
     public ParamSearchDto getAll() {
         return new ParamSearchDto("BMW", "530", "black", 2022);
+    }
+
+    @PutMapping("/{id}")
+    public ParamSearchDto updateParamAutoSearch(@PathVariable Integer id, @RequestBody ParamSearchDto paramSearchDto) {
+        return paramSearchDto;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteParamSearchById(@PathVariable Integer id) {
+
+    }
+
+    @PostMapping
+    public void createParamAutoSearch(@RequestBody ParamSearchDto paramSearchDto) {
+
     }
 }
